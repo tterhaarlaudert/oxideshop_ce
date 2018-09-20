299a6964c704b3f92e34c25eacca957beed34430
iegupov <igor.iegupov@gmail.com>
2018-09-20 17:21:01 +0200

    OXDEV-1284 Container trait for internal tests

---
+++ tests/Integration/Internal/ContainerTrait.php
@@ -0,0 +1,38 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * Copyright © OXID eSales AG. All rights reserved.
+ * See LICENSE file for license details.
+ */
+
+namespace OxidEsales\EshopCommunity\Tests\Integration\Internal;
+
+use OxidEsales\EshopCommunity\Internal\Application\ContainerBuilder;
+use Symfony\Component\DependencyInjection\ContainerBuilder as SymfonyContainerBuilder;
+
+/**
+ * @internal
+ */
+trait ContainerTrait
+{
+    private function get(string $serviceId)
+    {
+        $containerBuilder = new ContainerBuilder();
+        $container = $containerBuilder->getContainer();
+
+        $this->setContainerDefinitionToPublic($container, $serviceId);
+
+        $container->compile();
+
+        return $container->get($serviceId);
+    }
+
+    private function setContainerDefinitionToPublic(SymfonyContainerBuilder $container, string $definitionId)
+    {
+        $definition = $container->getDefinition($definitionId);
+        $definition->setPublic(true);
+
+        $container->setDefinition($definitionId, $definition);
+    }
+}
