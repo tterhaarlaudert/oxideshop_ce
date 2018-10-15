7aef850fea0ee49b7c5433a8aa5937cf8a3b7da5
hkreuter <heike@reuter-consultig.org>
2018-10-15 10:04:44 +0200

    fixup! 4339b269be093f1069649a70318f819ee2656868

--- source/Internal/Application/Events/SmartyFunctionIncludeWidgetEvent.php
+++ source/Internal/Application/Events/SmartyFunctionIncludeWidgetEvent.php
@@ -18,6 +18,23 @@ use Symfony\Component\EventDispatcher\Event;
 class SmartyFunctionIncludeWidgetEvent extends Event
 {
     /**
+     * Result
+     *
+     * @var string
+     */
+    protected $result = '';
+
+    /**
+     * @var Smarty
+     */
+    protected $smarty = null;
+
+    /**
+     * @var array
+     */
+    protected $parameters = [];
+
+    /**
      * Handle event.
      *
      * @return null
@@ -25,5 +42,65 @@ class SmartyFunctionIncludeWidgetEvent extends Event
     public function handleEvent()
     {
 
+    }
+
+    /**
+     * Setter for result.
+     *
+     * @param string $result
+     */
+    public function setResult($result)
+    {
+        $this->result = $result;
+    }
+
+    /**
+     * Setter for parameters.
+     *
+     * @param array $parameters Parameters
+     */
+    public function setParameters($parameters)
+    {
+        $this->parameters = $parameters;
+    }
+
+    /**
+     * Setter for Smarty.
+     *
+     * @param Smarty $smarty Smarty object
+     */
+    public function setSmarty($smarty)
+    {
+        $this->smarty = $smarty;
+    }
+
+    /**
+     * Getter for parameters
+     *
+     * @return array
+     */
+    public function getParameters()
+    {
+        return $this->parameters;
+    }
+
+    /**
+     * Getter for smarty.
+     *
+     * @return Smarty
+     */
+    public function getSmarty()
+    {
+        return $this->smarty;
+    }
+
+    /**
+     * Getter for result
+     *
+     * @return string
+     */
+    public function getResult()
+    {
+        return $this->result;
     }
 }
